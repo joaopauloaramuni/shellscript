3d5361e35652ef9f28cedfcf8fab8583c10336c2
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-11-05 20:11:53 -0300

    Delete exer18_parImparTOP.sh

--- exer18_parImparTOP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ler a variavél digitada pelo usuário. A opção -p originalmente mostra uma mensagem na saída de erro padrão, se você não estiver redirecionando essa saída para o vácuo (/dev/null) ou para nenhum outro lugar, será apresentada na tela do console normalmente.
-
-echo "Digite o número que queira saber se é par ou ímpar: "
-read num
-
-(( num % 2 == 0 )) && echo "Par" || echo "Impar"
